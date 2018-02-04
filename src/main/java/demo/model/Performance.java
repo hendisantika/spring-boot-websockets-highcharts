90d8a4d15630baabdaf091e6d4796b1968ac7cf9
Hendi Santika <hendisantika@gmail.com>
2018-02-04 08:06:00 +0700

    #5 - Add Performance Model

---
+++ src/main/java/demo/model/Performance.java
@@ -0,0 +1,82 @@
+package demo.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-websocket-highchart
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/02/18
+ * Time: 06.06
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Performance {
+
+    private long committedVirtualMemorySize;
+
+    private long totalSwapSpaceSize;
+    private long freeSwapSpaceSize;
+
+    private long totalPhysicalMemorySize;
+    private long freePhysicalMemorySize;
+
+    private double systemCpuLoad;
+    private double processCpuLoad;
+
+    public long getTotalSwapSpaceSize() {
+        return totalSwapSpaceSize;
+    }
+
+    public void setTotalSwapSpaceSize(long totalSwapSpaceSize) {
+        this.totalSwapSpaceSize = totalSwapSpaceSize;
+    }
+
+    public long getCommittedVirtualMemorySize() {
+        return committedVirtualMemorySize;
+    }
+
+    public void setCommittedVirtualMemorySize(long committedVirtualMemorySize) {
+        this.committedVirtualMemorySize = committedVirtualMemorySize;
+    }
+
+    public long getFreeSwapSpaceSize() {
+        return freeSwapSpaceSize;
+    }
+
+    public void setFreeSwapSpaceSize(long freeSwapSpaceSize) {
+        this.freeSwapSpaceSize = freeSwapSpaceSize;
+    }
+
+    public long getTotalPhysicalMemorySize() {
+        return totalPhysicalMemorySize;
+    }
+
+    public void setTotalPhysicalMemorySize(long totalPhysicalMemorySize) {
+        this.totalPhysicalMemorySize = totalPhysicalMemorySize;
+    }
+
+    public long getFreePhysicalMemorySize() {
+        return freePhysicalMemorySize;
+    }
+
+    public void setFreePhysicalMemorySize(long freePhysicalMemorySize) {
+        this.freePhysicalMemorySize = freePhysicalMemorySize;
+    }
+
+    public double getSystemCpuLoad() {
+        return systemCpuLoad;
+    }
+
+    public void setSystemCpuLoad(double systemCpuLoad) {
+        this.systemCpuLoad = systemCpuLoad;
+    }
+
+    public double getProcessCpuLoad() {
+        return processCpuLoad;
+    }
+
+    public void setProcessCpuLoad(double processCpuLoad) {
+        this.processCpuLoad = processCpuLoad;
+    }
+}
